a265ee4063b09d2a360ce9a3bc494595f675d27f
thanhnd <ducthanhnguyen95@gmail.com>
2023-06-17 13:26:41 +0700

    Overriding the endpoint authorization configuration

--- src/main/java/com/example/spring/security/first/project/config/ProjectConfig.java
+++ src/main/java/com/example/spring/security/first/project/config/ProjectConfig.java
@@ -2,11 +2,14 @@ package com.example.spring.security.first.project.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectConfig {
@@ -15,10 +18,7 @@ public class ProjectConfig {
     public UserDetailsService userDetailsService() {
         var userDetailsService = new InMemoryUserDetailsManager();
 
-        var user = User.withUsername("john")
-                .password("12345")
-                .authorities("read")
-                .build();
+        var user = User.withUsername("john").password("12345").authorities("read").build();
 
         userDetailsService.createUser(user);
 
@@ -29,4 +29,17 @@ public class ProjectConfig {
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        /*All the requests require authentication.*/
+        http.httpBasic(Customizer.withDefaults()).authorizeHttpRequests((authz) -> authz.anyRequest().authenticated());
+
+        /*None of the requests need to be authenticated.
+         * http.httpBasic(Customizer.withDefaults()).authorizeHttpRequests((authz) -> authz.anyRequest().permitAll());
+         * */
+
+        return http.build();
+    }
+
 }
