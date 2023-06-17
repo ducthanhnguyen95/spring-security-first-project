3d05b31d1f41391e31eb6b51763989a0bfdfee93
thanhnd <ducthanhnguyen95@gmail.com>
2023-06-17 16:51:51 +0700

    Overriding the AuthenticationProvider implementation

--- src/main/java/com/example/spring/security/first/project/config/ProjectConfig.java
+++ src/main/java/com/example/spring/security/first/project/config/ProjectConfig.java
@@ -1,33 +1,20 @@
 package com.example.spring.security.first.project.config;
 
+import com.example.spring.security.first.project.security.CustomAuthenticationProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectConfig {
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        var userDetailsService = new InMemoryUserDetailsManager();
+    private final CustomAuthenticationProvider authenticationProvider;
 
-        var user = User.withUsername("john").password("12345").authorities("read").build();
 
-        userDetailsService.createUser(user);
-
-        return userDetailsService;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+    public ProjectConfig(CustomAuthenticationProvider authenticationProvider) {
+        this.authenticationProvider = authenticationProvider;
     }
 
     @Bean
@@ -41,5 +28,4 @@ public class ProjectConfig {
 
         return http.build();
     }
-
 }
